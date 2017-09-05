66998b600ff170ffe086d746a26f6a6a631fb12a
MacMini <macmini@MacMini-Mac-mini.local>
2017-09-05 11:05:40 +0300

    Initial Commit

---
+++ proje/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  proje
+//
+//  Created by MacMini on 5.09.2017.
+//  Copyright © 2017 MacMini. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
